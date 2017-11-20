fd3ad30045d648893cbd4947eb100c8c8f5f7d89
Andres Borjas <aborjas@ufl.edu>
2017-11-20 18:15:16 -0500

    added way to click images, need to get so that it can get it dynamically though.

--- client/clientExample.js
+++ client/clientExample.js
@@ -9,6 +9,18 @@ window.addEventListener('resize', function(){		//keeps canvas to Event
 	canvas.height = window.innerHeight;
 });
 
+var mouse = {
+	x: undefined,
+	y: undefined
+}
+
+window.addEventListener('mousemove',function(event){
+	
+	mouse.x = event.x;
+	mouse.y = event.y;
+	
+});
+
 function reqListener () {
 	console.log(this.responseText);
 }
@@ -25,24 +37,25 @@ function Controller(view){
 	
 	window.addEventListener('click',function(event){
 		
+	if(mouse.x > 120 && mouse.x < 510 && mouse.y > 160 && mouse.y < 310) {	
 		
-		request.onreadystatechange = function () {
-			var DONE = this.DONE || 4;
-			if (this.readyState === DONE){
-				console.log(this);
-			}
-		};
-	request.open('POST', 'bish', true);
-	request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-	console.log("I did it bish is ready to go out.");
-	request.send();  // No data needs to be sent along with the 
+			request.onreadystatechange = function () {
+				var DONE = this.DONE || 4;
+				if (this.readyState === DONE){
+					console.log(this);
+				}
+			};
+		request.open('POST', 'bish', true);
+		request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+		console.log("I did it bish is ready to go out.");
+		request.send();  // No data needs to be sent along with the 
 	
-	view.displayImage('DogtectiveFinalTitle.png');
-	//view.display();
+		console.log('MouseX location:' + mouse.x);
+		console.log('MouseY location:' + mouse.y);
+		console.log('Start was pressed.');
 	
-	//model.modelChangeState();
-	//model.addPlayer();
-	//model.x.displayTest();
+	}	//if
+	
 	
 	});
 
@@ -54,7 +67,7 @@ function View(){
 		test_image = new Image();
 		test_image.src = 'client/assets/'+imageName;
 		test_image.onload = function() {
-			context.drawImage(test_image, 100, 100);
+			context.drawImage(test_image, 30, 30);
 		}
 		console.log("I should be displaying the image given " + imageName);
 	}
@@ -63,12 +76,57 @@ function View(){
 	//displayImage('DogtectiveFinalTitle.png');
 }
 
+function loadImages(sources, callback){
+	var images = {};
+	var loadedImages = 0;
+	var numImages = 0;
+	
+	for(var src in sources) {
+		images[src] = new Image();
+		images[src].onload = function() {
+			if(++loadedImages >=numImages) {
+				callback(images);
+			}
+		};
+		images[src].src = sources[src];
+	}
+}
+
+
+
 function main() {
 	console.log('Testing clientExample.');
 	
+	var sources = {
+		title: 'client/assets/DogtectiveFinalTitle.png',
+		pawPrint: 'client/assets/pawPrint.jpg',
+		start: 'client/assets/Start.png'
+	};
+	startX = 100;		//shit should be calculated and updated depending on screen size
+	startY = 80;
+	
+	this.getStartX = function(){
+		return startX;
+	}
+	this.getStartY = function(){
+		return startY;
+	}
+		
+		
 	
 	view = new View(),
 	controller = new Controller(view);
+	
+	loadImages(sources, function(images){
+		context.drawImage(images.title, 40, 30);
+		//context.drawImage(images.pawPrint, 350, 55, 93, 104);
+		context.drawImage(images.start, startX, startY);
+		//while I'm testing left bound is 120, right is 500
+		//up is 170
+		//down is 310
+	});
+	//view.displayImage('DogtectiveFinalTitle.png');
+	//view.displayImage('pawPrint.jpg');
 	
 	
 	
